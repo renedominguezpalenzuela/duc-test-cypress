4578073e9ee9b78c0a539b6779773b991568b192
rdominguez <rdominguez@soaint.com>
2023-10-27 15:36:20 -0500

    verificar numeror

--- cypress/e2e/phone-recharge.cy.js
+++ cypress/e2e/phone-recharge.cy.js
@@ -6,14 +6,29 @@ describe('Phone Recharge', function () {
     cy.get('#usr').type('darian.alvarez.t@gmail.com');
     cy.get('#pass').type('Buvosic8*');
     cy.get('#login-btn').click();
-    cy.contains("Phone Recharge", {timeout: Cypress.env('timeout')}).click();
+    cy.contains("Phone Recharge", { timeout: Cypress.env('timeout') }).click();
   })
 
   it('Carga pagina Phone Recharge correctamente', () => {
-    //cy.wait(10000); 
-    cy.contains("Currency to pay for recharge",  {timeout: Cypress.env('timeout')});   
+    cy.contains("Currency to pay for recharge", { timeout: Cypress.env('timeout') });
   })
 
- 
+  it('No se aceptan caracteres no numericos en el telefono 1', () => {
+    const cadena = 'abcdefg'
+    cy.get('#phone').type(cadena);
+    cy.get('#phone').should('have.value', '');
+  });
 
-})
+  it('No se aceptan caracteres no numericos en el telefono 2', () => {
+    const numero = 'abcd546ef57g345'
+    cy.get('#phone').type(numero);
+    cy.get('#phone').should('have.value', '54657345');
+  });
+
+  it('Introduciendo un numero de telefono correcto', () => {
+    const numero = '54657345'
+    cy.get('#phone').type(numero);
+    cy.get('#phone').should('have.value', '54657345');
+  });
+
+});
